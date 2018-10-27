8340203e439d40ea9648e4d4a886423c26987014
Varsha Dhawale <varsha.dhawale@outlook.com>
2018-10-27 15:16:07 +0530

    Add project files.

---
+++ CustomerOrderService.Tests/CustomerOrderServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CustomerOrderService;
+
+namespace CustomerOrderService.Tests
+{
+    [TestFixture]
+    public class CustomerOrderServiceTests
+    {
+        [TestCase]
+        public void When_PremiumCustomer_Expect_10PercentDiscount()
+        {
+            //Arrange
+            Customer premiumCustomer = new Customer
+            {
+                CustomerId = 1,
+                CustomerName = "George",
+                CustomerType = CustomerType.Premium
+            };
+
+            Order order = new Order
+            {
+                OrderId = 1,
+                ProductId = 212,
+                ProductQuantity = 1,
+                Amount = 150
+            };
+
+            CustomerOrderService customerOrderService = new CustomerOrderService();
+
+            //Act
+            customerOrderService.ApplyDiscount(premiumCustomer, order);
+
+            //Assert
+            Assert.AreEqual(order.Amount, 130);
+        }
+    }
+}
